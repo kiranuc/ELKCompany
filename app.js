57619e534591616d9cf356bdbdf78d4c0f08f0b1
kiranuc <149051444+kiranuc@users.noreply.github.com>
2024-04-12 11:04:26 +0530

    app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
